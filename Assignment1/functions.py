ffa6b1577441a1e99d4998c869d169aaed82d7bb
Vanshika030 <vanshikasoni166@gmail.com>
2026-07-08 15:37:56 +0530

    black and pylint done

--- Assignment1/functions.py
+++ Assignment1/functions.py
@@ -1,1 +1,28 @@
-print("hello")
+def add_numbers(a, b):
+    return a + b
+
+
+result = add_numbers(10, 20)
+print("Sum =", result)
+
+
+def check_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+
+print(check_even(8))
+print(check_even(15))
+
+
+def find_larger(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+result = find_larger(10, 20)
+print("Larger number =", result)
